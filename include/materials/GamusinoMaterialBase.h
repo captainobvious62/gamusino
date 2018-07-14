450241111a94b2d18c83f6cee89e8f563449e22b
captainobvious62 <rlwalker@usc.edu>
2018-07-14 01:48:23 -0700

    need to review C++ class structure

--- include/materials/GamusinoMaterialBase.h
+++ include/materials/GamusinoMaterialBase.h
@@ -23,8 +23,10 @@ class GamusinoMaterialBase : public Material
 {
 public:
   GamusinoMaterialBase(const InputParameters & parameters);
+  // Static method for use in validParams for getting the material type
   static MooseEnum materialType();
 
+/* -------------------------------------------------------------------------- */
 protected:
 
   virtual void initQpStatefulProperties();
@@ -40,24 +42,27 @@ protected:
 
   Function * _function_scaling;
 
+  // Initial (inputted) Values
+  Real _rho0_f;           // Initial fluid density, kg / m**3
+  Real _rho0_s;           // Initial solid density, kg / m**3
+  Real _phi0;             // Initial porosity, frac
+  Real _g;                // Gravitational acceleration, m / s**2
+  Real _scaling_factor0;  // Initial scaling factor for lower dimensional element, m
+  Real _alpha_T_f;        // Fluid thermal expansion, 1 / K
+  Real _alpha_T_s;        // Solid thermal expansion, 1 / K
 
-  Real _rho0_f;
-  Real _rho0_s;
-  Real _phi0;
-  Real _g;
-  Real _scaling_factor0;
-  Real _alpha_T_f;
-  Real _alpha_T_s;
+  // Flags
+  bool _has_gravity;      // Include gravity force flag
+  bool _has_scaled_properties; // Scale function flag
 
-  bool _has_gravity;
-  bool _has_scaled_properties;
-
+  // User Objects
   const GamusinoFluidDensity * _fluid_density_uo;
   const GamusinoFluidViscosity * _fluid_viscosity_uo;
   const GamusinoPermeability * _permeability_uo;
   const GamusinoPorosity * _porosity_uo;
   const GamusinoScaling * _scaling_uo;
 
+  // Choice from selection
   MooseEnum _material_type;
 
   // ===================
